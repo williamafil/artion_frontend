af8fe34cba37043e98645a73004535eb3840b369
ToroChu <whileonrails@gmail.com>
2021-04-14 11:22:46 +0800

    add created hook @main.js to persist data in vuex

--- src/main.js
+++ src/main.js
@@ -9,5 +9,12 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  created() {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const userData = JSON.parse(userString);
+      this.$store.commit('SET_USER_DATA', userData);
+    }
+  },
   render: (h) => h(App),
 }).$mount('#app');
