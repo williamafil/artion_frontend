f20e0b98df71c81867b36f9d78ef93cd7944c9eb
ToroChu <whileonrails@gmail.com>
2021-05-03 21:39:51 +0800

    modify api baseURL

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// 該檔案生成的方式可以在 Vue Ui 中的「設定」->「Vue Cli」->「公開路徑」找到。
-module.exports = {
-  // 預設狀況下，部署到 GitHub Pages 可能會無法顯示畫面
-  // 因此需要多設置該屬性
-  // 官方文件：https://cli.vuejs.org/zh/config/#publicpath
-  publicPath: './artion_frontend/',
-};
