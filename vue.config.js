05342a03be676dc1b55ac3a6386c8c65b37b7a9e
ToroChu <whileonrails@gmail.com>
2021-05-03 21:55:43 +0800

    add vue.config.js & modify sh deploy.sh

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// 該檔案生成的方式可以在 Vue Ui 中的「設定」->「Vue Cli」->「公開路徑」找到。
+module.exports = {
+  // 預設狀況下，部署到 GitHub Pages 可能會無法顯示畫面
+  // 因此需要多設置該屬性
+  // 官方文件：https://cli.vuejs.org/zh/config/#publicpath
+  publicPath: process.env.NODE_ENV === 'production' ? '/artion_frontend/' : '/',
+};
