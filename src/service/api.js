8b3d074fb38528bc2fc500d172a5e6783dfb2947
ToroChu <whileonrails@gmail.com>
2021-05-04 02:57:57 +0800

    api baseURL

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const apiService = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://artion.balliiballii.com',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://artion.balliiballii.com',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
