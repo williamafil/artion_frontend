ce83704c3f8f392cbae619e3683bb5a952f20f66
ToroChu <whileonrails@gmail.com>
2021-05-03 21:34:32 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = {
   // 預設狀況下，部署到 GitHub Pages 可能會無法顯示畫面
   // 因此需要多設置該屬性
   // 官方文件：https://cli.vuejs.org/zh/config/#publicpath
-  publicPath: './artion_frontend',
+  publicPath: './artion_frontend/',
 };
