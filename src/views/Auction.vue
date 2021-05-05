7cde75671591b2ca159371d04911eadfc73de147
ToroChu <whileonrails@gmail.com>
2021-05-05 08:37:29 +0800

    avatar url

--- src/views/Auction.vue
+++ src/views/Auction.vue
@@ -125,7 +125,7 @@
                   <div class="flex flex-col justify-center items-center mr-2 w-1/5">
                     <img
                       class="h-8 w-8 rounded-full object-cover"
-                      :src="bid.user.avatar_url ? bid.user.avatar_url : 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'"
+                      :src="!bid.user.avatar_url ? 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' : bid.user.avatar_url"
                       alt="頭像"
                     />
                   </div>
